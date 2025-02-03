26a281e3afd7cd5c36beed2f7c4a2581fcee24c9
harrygao56 <harrygao56@gmail.com>
2025-02-03 15:46:18 -0600

    switch to 4o

--- services/azure_openai.py
+++ services/azure_openai.py
@@ -5,8 +5,16 @@ from agent.get_secret import get_secret
 
 
 llm = AzureChatOpenAI(
+    deployment_name="gpt-4o",
+    openai_api_version="2024-08-01-preview",
+    azure_endpoint=get_secret("azure-openai-endpoint", "1"),
+    openai_api_key=get_secret("azure-openai-api-key", "1"),
+    temperature=0,
+)
+
+llm_4o_mini = AzureChatOpenAI(
     deployment_name="gpt-4o-mini",
-    openai_api_version="2023-03-15-preview",
+    openai_api_version="2024-08-01-preview",
     azure_endpoint=get_secret("azure-openai-endpoint", "1"),
     openai_api_key=get_secret("azure-openai-api-key", "1"),
     temperature=0,
@@ -16,6 +24,6 @@ llm = AzureChatOpenAI(
 def get_azure_openai() -> Optional[AzureOpenAI]:
     return AzureOpenAI(
         api_key=get_secret("azure-openai-api-key", "1"),
-        api_version="2023-03-15-preview",
+        api_version="2024-08-01-preview",
         azure_endpoint=get_secret("azure-openai-endpoint", "1"),
     )
